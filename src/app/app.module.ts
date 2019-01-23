4fe2b0c93a1467aaf736b0ddc05bf7a94a5896be
Thanh Le <nhatthanhh1993@gmail.com>
2019-01-23 09:37:05 +0200

    change name of branch : dev-register => dev-thanh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { RegisterPage } from "../pages/register/register";
+import { LoginPage } from "../pages/login/login";
+import { HomePage } from "../pages/home/home";
+import { PinsPage } from "../pages/pins/pins";
+import { MyItemsPage } from "../pages/my-items/my-items";
+import { UserInfoPage } from "../pages/user-info/user-info";
+import { PostPage } from "../pages/post/post";
+import { RepostPage } from "../pages/repost/repost";
+import { EditInfoPage } from "../pages/edit-info/edit-info";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    RegisterPage,
+    LoginPage,
+    HomePage,
+    PinsPage,
+    MyItemsPage,
+    UserInfoPage,
+    PostPage,
+    RepostPage,
+    EditInfoPage
   ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    RegisterPage,
+    LoginPage,
+    HomePage,
+    PinsPage,
+    MyItemsPage,
+    UserInfoPage,
+    PostPage,
+    RepostPage,
+    EditInfoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
