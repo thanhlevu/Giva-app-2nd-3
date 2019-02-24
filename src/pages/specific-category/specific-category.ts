7e3a1d4b61a2dfecfdc0c1e71422bed23146b5a1
Emil T <37114731+Em1lT@users.noreply.github.com>
2019-02-24 20:05:19 +0200

    update to category

--- src/pages/specific-category/specific-category.ts
+++ src/pages/specific-category/specific-category.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { Picture } from '../../intefaces/posting';
 
 @IonicPage()
 @Component({
@@ -12,6 +13,8 @@ export class SpecificCategoryPage {
   url = "https://media.mw.metropolia.fi/wbma/uploads/";
   thumbnail: string;
   selectedItem: string;
+  picArray: Picture[];
+  TrueArray: Picture[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
     this.selectedItem = navParams.get('category');
@@ -19,10 +22,26 @@ export class SpecificCategoryPage {
   }
 
   ionViewDidLoad() {
-    this.getItemsByTag();
+    this.loadItems_GivaTag();
   }
 
-  getItemsByTag(){
-
+  loadItems_GivaTag() {
+    return this.http
+      .get<Picture[]>("http://media.mw.metropolia.fi/wbma/tags/GIVA")
+      .subscribe(items => {
+        //order by the newest post
+        this.picArray = items.sort(
+          (a, b) => 
+          Number(b.file_id) - Number(a.file_id)
+          );
+          this.loadTrue();
+      });
+  }
+  loadTrue(){
+    this.picArray.forEach(data =>{
+      if(data.description.includes("description:")){
+      console.log(data.description.slice(data.description.indexOf("description"),data.description.indexOf("description:")+ 9))
+      }
+    })
   }
 }
